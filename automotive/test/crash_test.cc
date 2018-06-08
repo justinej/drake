b74d2ce67bc13485825da673a861b079c784b3cc
Justine J <justinej@mit.edu>
2018-06-08 00:39:36 -0400

    Fixed more bugs so simple_sim and crash_test behave the same way

--- automotive/test/crash_test.cc
+++ automotive/test/crash_test.cc
@@ -14,6 +14,9 @@
 #include "drake/systems/rendering/pose_bundle.h"
 
 namespace drake {
+
+using maliput::api::Lane;
+
 namespace automotive {
 namespace {
 #if GTEST_HAS_COMBINE
@@ -23,7 +26,7 @@ using ::testing::Bool;
 using ::testing::Values;
 using ::testing::Combine;
 
-class CrashTest2 : public TestWithParam< ::testing::tuple<double, double, double, double> > {
+class CrashTest : public TestWithParam< ::testing::tuple<double, double, double, double> > {
  protected:
   virtual void SetUp() {
     v1 = ::testing::get<0>(GetParam());
@@ -38,54 +41,90 @@ class CrashTest2 : public TestWithParam< ::testing::tuple<double, double, double
   double distance;
 };
 
-// TEST_P(CrashTest2, CorrectlyReadsParameters) {
-//     EXPECT_GT(v1, 0);
-//     EXPECT_GT(v2, 0);
-//     EXPECT_GT(accel, 0);
-//     EXPECT_GT(distance, 0);
+// TEST_P(CrashTest, CorrectlyReadsParameters) {
+//     EXPECT_EQ(v1, 40);
+//     EXPECT_EQ(v2, 40);
+//     EXPECT_EQ(accel, -5);
+//     EXPECT_EQ(distance, 2);
 // }
 
-TEST_P(CrashTest2, ReadsSimpleCarState) {
+const maliput::api::RoadGeometry* AddDragway(
+    AutomotiveSimulator<double>* simulator) {
+  const double kMaximumHeight = 5.;  // meters
+  const double kLinearTolerance = std::numeric_limits<double>::epsilon();
+  const double kAngularTolerance = std::numeric_limits<double>::epsilon();
+  std::unique_ptr<const maliput::api::RoadGeometry> road_geometry
+      = std::make_unique<const maliput::dragway::RoadGeometry>(
+          maliput::api::RoadGeometryId("Automotive Demo Dragway"),
+          1 /* num dragway lanes */,
+          300 /* dragway length */,
+          3.7 /*lane width */,
+          3.0 /* shoulder length */,
+          kMaximumHeight, kLinearTolerance, kAngularTolerance);
+  return simulator->SetRoadGeometry(std::move(road_geometry));
+}
+
+int AddSimpleCar(AutomotiveSimulator<double>* simulator,
+                  const double distance,
+                  const double velocity,
+                  const double acceleration) {
+  SimpleCarState<double> initial_state_car1;
+  initial_state_car1.set_x(distance);
+  initial_state_car1.set_y(0);
+  initial_state_car1.set_velocity(velocity);
+  initial_state_car1.set_acceleration(acceleration);
+  const int car1_id = simulator->AddPriusSimpleCar(
+      "SimpleCar", "SimpleCarChannel", initial_state_car1);
+  return car1_id;
+}
+
+int AddIdmControlledMaliputRailcar(AutomotiveSimulator<double>* simulator,
+                                    const double velocity) {
+  const maliput::api::RoadGeometry* road = AddDragway(simulator);
+  const maliput::dragway::RoadGeometry* dragway_road_geometry =
+      dynamic_cast<const maliput::dragway::RoadGeometry*>(road);
+  const Lane* lane =
+        dragway_road_geometry->junction(0)->segment(0)->lane(0);
+
+  const MaliputRailcarParams<double> params;
+  MaliputRailcarState<double> initial_state_car2;
+  initial_state_car2.set_speed(velocity);
+
+  const int car2_id = simulator->AddIdmControlledPriusMaliputRailcar(
+      "IdmControlledMaliputCar",
+      LaneDirection(lane),
+      ScanStrategy::kPath,
+      RoadPositionStrategy::kExhaustiveSearch,
+      0. /* time period (unseen) */,
+      params,
+      initial_state_car2);
+  return car2_id;
+}
+
+void CompareDistances(const systems::rendering::PoseBundle<double>& poses,
+                       const int kCar1Index, const int kCar2Index) {
+  const Isometry3<double>& pose_1 = poses.get_pose(kCar1Index);
+  const Isometry3<double>& pose_2 = poses.get_pose(kCar2Index);
+  drake::log()->info("Pose 1 translation x is {}", pose_1.translation().x());
+  drake::log()->info("Pose 2 translation x is {}", pose_2.translation().x());
+  EXPECT_LT(pose_2.translation().x() + 5.0, pose_1.translation().x());
+}
+
+TEST_P(CrashTest, ReadsSimpleCarState) {
   auto simulator = std::make_unique<AutomotiveSimulator<double>>(
       std::make_unique<lcm::DrakeMockLcm>());
   const double kX_car1{distance};
-  const double kY_car1{0};
   const double kVelocity_car1{v1};
   const double kVelocity_car2{v2};
-  const double kCar1Acceleration{accel};
+  const double kAcceleration_car1{accel};
   const int kCar1Index{0};
   const int kCar2Index{1};
 
-  SimpleCarState<double> initial_state_car1;
-  initial_state_car1.set_x(kX_car1);
-  initial_state_car1.set_y(kY_car1);
-  initial_state_car1.set_velocity(kVelocity_car1);
-  initial_state_car1.set_acceleration(kCar1Acceleration);
-  const int car1_id = simulator->AddPriusSimpleCar(
-      "Car1", "Channel", initial_state_car1);
-
-  const MaliputRailcarParams<double> params;
-  const maliput::api::RoadGeometry* road =
-      simulator->SetRoadGeometry(
-          std::make_unique<const maliput::dragway::RoadGeometry>(
-              maliput::api::RoadGeometryId("TestDragway"), 1 /* num lanes */,
-              10000 /* length */, 4 /* lane width */, 1 /* shoulder width */,
-              5 /* maximum_height */,
-              std::numeric_limits<double>::epsilon() /* linear_tolerance */,
-              std::numeric_limits<double>::epsilon() /* angular_tolerance */));
-  MaliputRailcarState<double> initial_state_car2;
-  initial_state_car2.set_speed(kVelocity_car2);
-  const int car2_id = simulator->AddPriusMaliputRailcar("Car2",
-      LaneDirection(road->junction(0)->segment(0)->lane(0)), params,
-      initial_state_car2);
+  const int car1_id = AddSimpleCar(simulator.get(), kX_car1, kVelocity_car1, kAcceleration_car1);
+  const int car2_id = AddIdmControlledMaliputRailcar(simulator.get(), kVelocity_car2);
 
   simulator->Start(100.0); // target_realtime_rate
-  
-  simulator->StepBy(1.0);
-  simulator->StepBy(1.0);
-  simulator->StepBy(1.0);
-  simulator->StepBy(1.0);
-  // simulator->StepBy(1.0);
+  simulator->StepBy(10.0);
 
   const systems::rendering::PoseBundle<double> poses =
       simulator->GetCurrentPoses();
@@ -93,10 +132,7 @@ TEST_P(CrashTest2, ReadsSimpleCarState) {
   ASSERT_EQ(poses.get_model_instance_id(kCar1Index), car1_id);
   ASSERT_EQ(poses.get_model_instance_id(kCar2Index), car2_id);
   
-  const Isometry3<double>& pose_1 = poses.get_pose(kCar1Index);
-  const Isometry3<double>& pose_2 = poses.get_pose(kCar2Index);
-  drake::log()->info("Hi {}", pose_2.translation().x());
-  EXPECT_LT(pose_2.translation().x() + 5.0, pose_1.translation().x());
+  CompareDistances(poses, kCar1Index, kCar2Index);
   // 5.0 is the length of the car
 }
 
@@ -112,11 +148,11 @@ TEST_P(CrashTest2, ReadsSimpleCarState) {
 // PrecalcPrimeTable instance and some inside it (10). Combine will produce all
 // possible combinations.
 INSTANTIATE_TEST_CASE_P(MeaningfulTestParameters,
-                        CrashTest2,
-                        Combine(Values(40),     // velocities for car 1
-                                Values(40),     // velocities for car 2
-                                Values(-5),   // acceleration of car 1
-                                Values(2)));     // distance between the cars
+                        CrashTest,
+                        Combine(Values(50),     // velocities for car 1
+                                Values(50),     // velocities for car 2
+                                Values(-10),     // acceleration of car 1
+                                Values(50)));     // distance between the cars
 
 #else
 
